da85d140f70c9d48bb9aa7a4a52012ab2c76b691
Preethi R <preetr19@in.ibm.com>
2019-11-22 10:40:51 +0530

    done autoincremental

---
+++ my-spring-cloud-api-service/my-spring-cloud-api-service/src/main/java/com/myapp/spring/MySpringCloudApiServiceApplication.java
@@ -0,0 +1,33 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MySpringCloudApiServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringCloudApiServiceApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	CorsConfiguration config = new CorsConfiguration();
+	config.setAllowCredentials(true);
+	config.addAllowedOrigin("*");
+	config.addAllowedHeader("*");
+	config.addAllowedMethod("*");
+	source.registerCorsConfiguration("/**", config);
+	FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	bean.setOrder(0);
+	return bean;
+	}
+}
